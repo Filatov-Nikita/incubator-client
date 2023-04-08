39dae89349c6391c877fea84978ffce0298e4c73
Nikita Antonov <nikita45454@gmail.com>
2023-04-08 13:16:56 +0500

    fix: убирает баг предзагрузки данных

--- src/boot/load-data.ts
+++ src/boot/load-data.ts
@@ -3,20 +3,33 @@ import { Notify, Loading } from 'quasar';
 import { useCategoriesStore } from 'src/stores/categories';
 import { useTagsStore } from 'src/stores/tags';
 
-export default boot(async ({ app }) => {
-  try {
-    Loading.show();
+export default boot(async ({ router }) => {
+
+  router.beforeEach(async (to, from, next) => {
+    if(to.name === 'login') return next();
+
     const catsStore = useCategoriesStore();
     const tagsStore = useTagsStore();
 
-    await Promise.all([
-      catsStore.list(),
-      tagsStore.list()
-    ]);
+    if(catsStore.items !== null && tagsStore.items !== null) {
+      return next();
+    }
 
-  } catch(e) {
-    Notify.create({ type: 'negative', message: 'Не удалось загрузить данные' });
-  } finally {
-    Loading.hide();
-  }
+    try {
+      Loading.show();
+
+      await Promise.all([
+        catsStore.list(),
+        tagsStore.list()
+      ]);
+
+      return next();
+
+    } catch(e) {
+      Notify.create({ type: 'negative', message: 'Не удалось загрузить данные' });
+    } finally {
+      Loading.hide();
+    }
+  });
+
 });
