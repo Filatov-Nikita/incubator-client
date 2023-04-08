a9257bd20f366b7c4b72a78d64ec30524ace47aa
Nikita Antonov <nikita45454@gmail.com>
2023-04-08 12:51:06 +0500

    feat: добаляет реакцию на проваленную авторизацию

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,24 @@
 import ky from 'ky';
 import { API } from 'src/config/app';
+import * as Tokens from 'src/core/tokens';
 
-export const api = ky.create({ prefixUrl: API });
+export const api = ky.create({
+  prefixUrl: API,
+  hooks: {
+    beforeRequest: [
+      (req) => {
+        const token = Tokens.get();
+        if(token) req.headers.append('token', token);
+      }
+    ],
+    afterResponse: [
+      (req, opts, res) => {
+        if(res.status === 401) {
+          Tokens.clean();
+          const cb = (api as any).onAuthFailed;
+          cb && cb();
+        }
+      }
+    ]
+  }
+});
--- src/boot/init.ts
+++ src/boot/init.ts
@@ -1,5 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import { API } from 'src/config/app';
+import { api } from 'src/api/index';
 
 declare module '@vue/runtime-core' {
   export interface ComponentCustomProperties {
@@ -13,7 +14,9 @@ function prettyNumber(number: number) {
   return number.toLocaleString().replace(',', ' ');
 }
 
-export default boot(({ app }) => {
+export default boot(({ app, router }) => {
+  (api as any).onAuthFailed = () => router.replace({ name: 'login' });
+
   app.config.globalProperties.$imgPath = function (path, fallback = '') {
     if(path === null) return fallback;
     return `${API}${path}`;
