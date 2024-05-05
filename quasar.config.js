1ad21701410da26a687d90dcc06f6b8e5944a381
Nikita Antonov <nikita45454@gmail.com>
2024-05-05 17:30:12 +0500

    устанавливает throttle-debounce

--- quasar.config.js
+++ quasar.config.js
@@ -78,6 +78,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      port: 8090,
       open: true // opens browser window automatically
     },
 
